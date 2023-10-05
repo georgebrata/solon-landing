e1d8e5ddb94f6a9b9e5163bda5770fbdf8ddee67
George Brata <bratageorge@gmail.com>
2023-10-05 10:05:59 +0300

    Added mesaj-trimis page

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -10,44 +10,51 @@
 
   forms.forEach( function(e) {
     e.addEventListener('submit', function(event) {
-      event.preventDefault();
+      // console.log(document.getElementsByClassName('sent-message'))
+      document.getElementsByClassName('sent-message')[0].style = 'display: block;';
+      document.getElementsByClassName('trimite-btn')[0].disabled = 'true';
+    })
+  })
+  // forms.forEach( function(e) {
+  //   e.addEventListener('submit', function(event) {
+  //     event.preventDefault();
 
-      let thisForm = this;
+  //     let thisForm = this;
 
-      let action = thisForm.getAttribute('action');
-      let recaptcha = thisForm.getAttribute('data-recaptcha-site-key');
+  //     let action = thisForm.getAttribute('action');
+  //     let recaptcha = thisForm.getAttribute('data-recaptcha-site-key');
       
-      if( ! action ) {
-        displayError(thisForm, 'The form action property is not set!');
-        return;
-      }
-      thisForm.querySelector('.loading').classList.add('d-block');
-      thisForm.querySelector('.error-message').classList.remove('d-block');
-      thisForm.querySelector('.sent-message').classList.remove('d-block');
+  //     if( ! action ) {
+  //       displayError(thisForm, 'The form action property is not set!');
+  //       return;
+  //     }
+  //     thisForm.querySelector('.loading').classList.add('d-block');
+  //     thisForm.querySelector('.error-message').classList.remove('d-block');
+  //     thisForm.querySelector('.sent-message').classList.remove('d-block');
 
-      let formData = new FormData( thisForm );
+  //     let formData = new FormData( thisForm );
 
-      if ( recaptcha ) {
-        if(typeof grecaptcha !== "undefined" ) {
-          grecaptcha.ready(function() {
-            try {
-              grecaptcha.execute(recaptcha, {action: 'php_email_form_submit'})
-              .then(token => {
-                formData.set('recaptcha-response', token);
-                php_email_form_submit(thisForm, action, formData);
-              })
-            } catch(error) {
-              displayError(thisForm, error);
-            }
-          });
-        } else {
-          displayError(thisForm, 'The reCaptcha javascript API url is not loaded!')
-        }
-      } else {
-        php_email_form_submit(thisForm, action, formData);
-      }
-    });
-  });
+  //     if ( recaptcha ) {
+  //       if(typeof grecaptcha !== "undefined" ) {
+  //         grecaptcha.ready(function() {
+  //           try {
+  //             grecaptcha.execute(recaptcha, {action: 'php_email_form_submit'})
+  //             .then(token => {
+  //               formData.set('recaptcha-response', token);
+  //               php_email_form_submit(thisForm, action, formData);
+  //             })
+  //           } catch(error) {
+  //             displayError(thisForm, error);
+  //           }
+  //         });
+  //       } else {
+  //         displayError(thisForm, 'The reCaptcha javascript API url is not loaded!')
+  //       }
+  //     } else {
+  //       php_email_form_submit(thisForm, action, formData);
+  //     }
+  //   });
+  // });
 
   function php_email_form_submit(thisForm, action, formData) {
     fetch(action, {
