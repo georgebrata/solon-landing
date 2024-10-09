23502cee98bb465c8856b63810e29790ee8924a6
George Brata <bratageorge@gmail.com>
2024-10-09 12:46:20 +0300

    Small updates

--- assets/js/price-toggle.js
+++ assets/js/price-toggle.js
@@ -16,7 +16,7 @@
     function clickMonthly() {
         select('#price-bronze').textContent = '99/lună';
         select('#price-silver').textContent = '199/lună';
-        select('#price-gold').textContent = '399/lună';
+        select('#price-gold').textContent = '349/lună';
 
         select('#link-bronze').href = 'https://buy.stripe.com/eVa4gI8jaagyb16dQV';
         select('#link-silver').href = 'https://buy.stripe.com/28o8wY1UMcoG1qwaEH';
@@ -26,7 +26,7 @@
     function clickAnnual() {
         select('#price-bronze').textContent = '999/an';
         select('#price-silver').textContent = '1,999/an';
-        select('#price-gold').textContent = '3,799/an';
+        select('#price-gold').textContent = '2,999/an';
 
         select('#link-bronze').href = 'https://buy.stripe.com/7sI00sfLC88qb1614c';
         select('#link-silver').href = 'https://buy.stripe.com/dR6aF60QI60iedi5kr';
